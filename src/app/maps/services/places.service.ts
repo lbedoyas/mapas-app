6ac6e444b7b62165d50ae307dc1d920910c8136c
Luis Bedoya <luis.bedoya@bhdleon.com.do>
2022-02-06 20:10:26 -0500

    primer commit
    primer commit

---
+++ src/app/maps/services/places.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  public useLocation?:[number, number];
+
+  get IsUserLocationReady(): boolean{
+    return !!this.useLocation;
+  }
+
+  constructor() {
+    this.getUserLocation();
+  }
+
+  public async getUserLocation(): Promise<[number, number]> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          this.useLocation = [coords.longitude, coords.latitude];
+          resolve(this.useLocation)
+        },
+        (err) => {
+          alert('No se pudo tener la geolocalizacion');
+          console.error(err);
+          reject()
+        }
+      )
+    });
+  }
+
+}
